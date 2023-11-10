d881d83cfb029b026bc652c90b5382dec61f393f
j Han <104748178+SGTkiwi@users.noreply.github.com>
2023-11-11 01:41:27 +1300

    Initialize sprint10

---
+++ routes/__init__.py
@@ -0,0 +1,9 @@
+from routes.account import blp as AccountBlueprint
+from routes.profile import blp as ProfileBlueprint
+from routes.cv import blp as CvBlueprint
+from routes.job_post import blp as JobPostBlueprint
+from routes.user_membership import blp as UserMembershipBlueprint
+from routes.user_application_management import blp as UserApplicationBlueprint
+from routes.business_application_management import blp as BusinessApplicantBlueprint
+from routes.jobs import blp as JobsBlueprint
+from routes.bookmark import blp as BookmarkBlueprint
